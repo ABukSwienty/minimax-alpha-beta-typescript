550c8628e33a024dbde52b3230be0af31df169d0
ABukSwienty <alexanderbukswienty@gmail.com>
2022-06-28 11:21:18 +0200

    gh pages

--- build/static/js/react-syntax-highlighter_languages_refractor_brainfuck.c4d33159.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkminimax=self.webpackChunkminimax||[]).push([[5539],{2689:function(a){function e(a){a.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}a.exports=e,e.displayName="brainfuck",e.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.c4d33159.chunk.js.map
